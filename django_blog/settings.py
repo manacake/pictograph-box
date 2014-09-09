e2526f0ac6bbd8971b57c36f8417fd82c9866964
Tina Zhao <tinayzhao@gmail.com>
2014-09-09 10:11:07 -0700

    frontend facelift

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 
 # Application definition
